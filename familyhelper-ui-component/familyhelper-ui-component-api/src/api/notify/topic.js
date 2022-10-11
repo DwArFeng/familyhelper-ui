b59297105a9ed73e6767e8434389b9ea0b8a6db8
DwArFeng <915724865@qq.com>
2022-10-11 21:27:53 +0800

    优化页面
    通知管理 -> 主题设置增加新字段的显示与编辑
    
    Signed-off-by: DwArFeng <915724865@qq.com>

--- familyhelper-ui-component/familyhelper-ui-component-api/src/api/notify/topic.js
+++ familyhelper-ui-component/familyhelper-ui-component-api/src/api/notify/topic.js
@@ -8,7 +8,9 @@ export function inspect(key) {
     return get('notify', `topic/${key}/`, {});
 }
 
-export function insert(key, label, enabled, priority, preferred, coolDownDuration, remark) {
+export function insert(
+    key, label, enabled, priority, preferred, coolDownDuration, executorType, executorParam, remark
+) {
     return post('notify', 'topic/', {
         key: {
             string_id: key,
@@ -18,6 +20,8 @@ export function insert(key, label, enabled, priority, preferred, coolDownDuratio
         priority,
         preferred,
         cool_down_duration: coolDownDuration,
+        executor_type:executorType,
+        executor_param:executorParam,
         remark,
     });
 }
@@ -26,7 +30,9 @@ export function remove(key) {
     return del('notify', `topic/${key}/`, {});
 }
 
-export function update(key, label, enabled, priority, preferred, coolDownDuration, remark) {
+export function update(
+    key, label, enabled, priority, preferred, coolDownDuration,executorType, executorParam, remark
+) {
     return patch('notify', 'topic/', {
         key: {
             string_id: key,
@@ -36,6 +42,8 @@ export function update(key, label, enabled, priority, preferred, coolDownDuratio
         priority,
         preferred,
         cool_down_duration: coolDownDuration,
+        executor_type:executorType,
+        executor_param:executorParam,
         remark,
     });
 }
