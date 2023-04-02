b57f7ded63f58c7d78882c2a6cbf33afcafbb819
DwArFeng <915724865@qq.com>
2023-04-02 20:26:20 +0800

    实现日常生活一级菜单
    活动类型管理二级菜单
    活动模板驱动支持二级菜单
    
    Signed-off-by: DwArFeng <915724865@qq.com>

--- familyhelper-ui-view/familyhelper-ui-view-pc/src/vim/items/life.js
+++ familyhelper-ui-view/familyhelper-ui-view-pc/src/vim/items/life.js
@@ -99,6 +99,74 @@ const life = [
       parentKey: 'life',
     },
   },
+  {
+    key: 'life.activityTypeIndicator',
+    index: 40,
+    meta: {
+      display: {
+        iconType: 'elementUI',
+        iconContent: '',
+        labelType: 'elementUI',
+        labelContent: '活动类型管理',
+      },
+      menu: {
+        shown: true,
+      },
+      ezNav: {
+        shown: true,
+        closedBehavior: {
+          type: 'back',
+          data: null,
+        },
+      },
+      router: {
+        required: true,
+        path: 'app/life/activity-type-indicator',
+        component: () => import('@/views/items/life/activityTypeIndicator/ActivityTypeIndicator.vue'),
+      },
+      permission: {
+        required: true,
+        node: 'ui.pc.menu_visible.life.activity_type_indicator',
+      },
+    },
+    node: {
+      parentKey: 'life',
+    },
+  },
+  {
+    key: 'life.activityTemplateDriverSupport',
+    index: 70,
+    meta: {
+      display: {
+        iconType: 'elementUI',
+        iconContent: '',
+        labelType: 'elementUI',
+        labelContent: '活动模板驱动支持',
+      },
+      menu: {
+        shown: true,
+      },
+      ezNav: {
+        shown: true,
+        closedBehavior: {
+          type: 'back',
+          data: null,
+        },
+      },
+      router: {
+        required: true,
+        path: 'app/life/activity-template-driver-support',
+        component: () => import('@/views/items/life/activityTemplateDriverSupport/ActivityTemplateDriverSupport.vue'),
+      },
+      permission: {
+        required: true,
+        node: 'ui.pc.menu_visible.life.activity_template_driver_support',
+      },
+    },
+    node: {
+      parentKey: 'life',
+    },
+  },
 ];
 
 export default life;
