fbc69f39d2830d1e0b2f901ff90b9570382b63b1
DwArFeng <915724865@qq.com>
2024-07-29 23:10:13 +0800

    增加组件功能
    Header 布局面板增加 applyContainerHeight 属性，以控制组件的高度是否自适应，该更新属于不兼容更新
    
    Signed-off-by: DwArFeng <915724865@qq.com>

--- familyhelper-ui-view/familyhelper-ui-view-pc/src/components/layout/HeaderLayoutPanel.vue
+++ familyhelper-ui-view/familyhelper-ui-view-pc/src/components/layout/HeaderLayoutPanel.vue
@@ -1,19 +1,29 @@
 <template>
-  <div class="header-layout-container">
-    <slot name="header"/>
+  <div class="header-layout-panel-container">
+    <div class="header-container-wrapper">
+      <slot name="header"/>
+    </div>
     <el-divider v-if="$slots.header"/>
-    <slot name="default"/>
+    <div class="main-container-wrapper" :class="{expand:!applyContainerHeight}">
+      <slot name="default"/>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HeaderLayoutPanel',
+  props: {
+    applyContainerHeight: {
+      type: Boolean,
+      default: false,
+    },
+  },
 };
 </script>
 
 <style scoped>
-.header-layout-container {
+.header-layout-panel-container {
   width: 100%;
   height: 100%;
   display: flex;
@@ -23,5 +33,15 @@ export default {
 /*noinspection CssUnusedSymbol*/
 .el-divider {
   margin: 5px 0;
+}
+
+.main-container-wrapper {
+  width: 100%;
+}
+
+/*noinspection CssUnusedSymbol*/
+.main-container-wrapper.expand {
+  height: 0;
+  flex-grow: 1;
 }
 </style>
