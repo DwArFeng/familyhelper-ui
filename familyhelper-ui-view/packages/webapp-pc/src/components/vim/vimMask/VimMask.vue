8a0f30ff1d526a2ec84141a2af747bbf730d4161
DwArFeng <915724865@qq.com>
2026-03-14 00:13:13 +0800

    webapp-pc 子模块功能优化
    实现可视化器的动态加载
    
    Signed-off-by: DwArFeng <915724865@qq.com>

---
+++ familyhelper-ui-view/packages/webapp-pc/src/components/vim/vimMask/VimMask.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="vim-mask placeholder">
+    <div>Powered by VIM 5</div>
+    <div>第 5 代前端框架</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'VimMask',
+})
+</script>
+
+<style scoped>
+.vim-mask {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.placeholder {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+  font-weight: bold;
+  color: #bfbfbf;
+  user-select: none;
+}
+</style>
